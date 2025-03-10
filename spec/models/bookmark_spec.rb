840d14789a5c601e01f201247f17cb2681582e97
Vitali Kharevich <kharevich.vitali@gmail.com>
2025-03-10 07:33:10 +0300

    improve bookmark model tests

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  subject { described_class.new() }
+  subject { described_class.new(title: "Google", url: "https://google.com") }
 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:url) }
 
   it "validates url format is correct" do
+    expect(subject).to be_valid
+
     subject.url = "google.com"
     expect(subject).to be_invalid
   end
